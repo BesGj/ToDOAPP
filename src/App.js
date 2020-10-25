329b1c024a2805501bed3145c1e885b0bfedf2c5
BesGj <72765141+BesGj@users.noreply.github.com>
2020-10-25 22:31:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+export default function App() {
+  return (
+    <div className="App">
+      
+      <TodoList />
+    </div>
+  );
+}
